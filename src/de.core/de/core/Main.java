ec3fcaf53789df1a178d9a89d41dea18a7f0d4c0
Michel Wohlert <michel.wohlert@gmail.com>
2016-11-28 11:27:02 +0100

    Update Main.java
    helped formatting for use in presentation

--- src/de.core/de/core/Main.java
+++ src/de.core/de/core/Main.java
@@ -8,7 +8,8 @@ import java.util.ServiceLoader;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("--- START ---");
-		Iterator<Message> messageIterator = ServiceLoader.load(Message.class).iterator();
+		Iterator<Message> messageIterator = ServiceLoader.load(Message.class)
+			.iterator();
 		while (messageIterator.hasNext()){
 			Message message = messageIterator.next();
 			System.out.println(message.getMessage());
